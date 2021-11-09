5269c1a207883d9f5d692f6068729431bd5355c0
BotEvie <91641915+BotEvie@users.noreply.github.com>
2021-11-08 23:53:05 -0500

    Update Threshold.c
    Changed update_threshold_value() to accept and use arrays.  Added some guiding notes for what to do next for myself. Other general edits to variables and their names.

--- Threshold.c
+++ Threshold.c
@@ -6,58 +6,67 @@
 #include <stm32l053xx.h>
 #include <stdint.h>
 
-void update_threshold_value(int toast);
-void threshold_fsm(int value);
-
-
-// 
-// Value 3 is more acceleration (higher voltage input)
-// 
-// +- 16384 is max value 
-//
-// x = (x > 0)? x: -x;
-//
-//
+int update_threshold_value(int toast);
+void threshold_fsm(int *accel_queue_head, int *value_queue_head);
 
 // Tested output accelerometer system total = T.O.A.S.T.
-// TODO: change this to accept and test an array of 3 integers. Then return the highest value.
-void update_threshold_value(int toast)
+// Done: change this to accept and test an array of 3 integers. Find and return the highest value of acceleration.
+int update_threshold_value(int *toast)
 {
-  	toast = (toast > 0)? toast: -toast;
-  
-	if(toast > 12000) { value = 3; }
+	for(int i=0; i<(sizeof(toast)/sizeof(toast[0])); i++)
+	{
+		if(toast[i] > value){ value = toast[i]; }
+	}	
+
+	
+	// Higher numbers indicate higher acceleration
+  	value = (value > 0)? value: -value;
+	if(toast > 12000){ value = 3; }
 	if(toast > 8000){ value = 2; }
 	if(toast > 4000){ value = 1; }
 	else{ value = 0; }
   	return value;
 }
 
-void threshold_fsm()
+
+
+void threshold_fsm(int *accel_queue_head, int *value_queue_head)
 {
-	static int state;
-	static int value;
-	static int max;
+	volatile int threshold_state;
+	volatile int threshold_max;
+	int value = 0;
 	int data[3];
 	
 	// TODO: get acceleration data from external queue. Assign it to data and feed to update_threshold_value()
-	value = update_threshold_value(toast);
+	// int data[3] = pop(accel_queue_head, queue_tail);?
+	value = update_threshold_value(data);
 		
 	/* state: Motion the box is undergoing. Has 4 seperate values.
 	 	0: box is not accelerating.
 		1: box may be accelerating, wait for another reading.
 	 	2: box is accelerating. Start recording values to find max.
-		3: box may have decelarated below threshold. Await further input to confirm.		
+		3: box may have decelarated below threshold. Await further input to confirm.
+		
+	Something to Note: 
+		This statemachine will not allow output until the object has stopped or 
+		until acceleration has dropped to a low enough point. I can see about fixing this
+		in a later iteration but this will do for now.
+		
+	TODO: update this to compare the value to max rather than a fixed value.
+	This will give us a peak value and allow us to determine that the acceleration spike is falling, and
+	fix the problem of no output until no acceleration.
 	*/	
 	switch(state)
 	{
-		case (0) : if(value > 0){ state = 1; max = 0; }
+		case (0) : if(value > 0){ state = 1; max = 0; }		// 
 			break;
-		case (1) : if(value > 0){ state = 2; max = value;}
-			if(value < 0){ state = 0; }	
+		case (1) : if(value > 0){ state = 2; max = value; }	//
+			if(value < 1){ state = 0; }			//
 			break;
-		case (2) : if(value < 0){ state = 3; if(max < value) {max = value;} }
+		case (2) : if(value < 1){ state = 3; }
+			 if(max < value) {max = value;}		
 			break;
-		case (3) : if(value < 0){ state = 0; //TODO: output max to external IPC here  }
+		case (3) : if(value == 0){ state = 0; /*TODO: output max to external IPC here. (push(value_queue_head))? */   }
 			if(value > 0){ state = 2; if(max < value) {max = value;} }
 			break;
 		default : state = 0;
