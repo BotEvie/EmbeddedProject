39ec3b8ee6c49b973dddd66c47ef6bce262ccb8a
Dru walkowski <91984465+dawalkowski17@users.noreply.github.com>
2021-11-09 14:50:46 -0500

    Change more things
    gpio changes from adc to spi

--- init_everything.c
+++ init_everything.c
@@ -13,28 +13,50 @@ void init_timer2(void);
 void init_spi()
 { // accelerometer uses spi or I2C and I don't really know how to use I2C
 	
+	RCC->APB2ENR |= 1 << (RCC_APB2ENR_SPI1EN_Pos);
+	//bit 11, 8-bit package
+	//bit 9= 1, ssm
+	//bit 8= 1, ssi
+	//bit 7= 0, MSB first
+	//bit 6= 1, SPI on
+	//bits[5:3], 11MHz max on data transfer
+	//bit 2= 1, MOSI first will eventually change to slave
+	//bit 1= 1, CPOL
+	//bit 0= 1, CPHA
+	SPI2->CR1 |= 1 << (SPI_CR1_DFF_Pos);
+	SPI2->CR1 &= ~(7u << (SPI_CR1_BR_Pos));
+	SPI2->CR1 |= 1 << (SPI_CR1_SSM_Pos);
+	SPI2->CR1 |= 1 << (SPI_CR1_SSI_Pos);
+	SPI2->CR1 |= 1 << (SPI_CR1_MSTR_Pos);
+	SPI2->CR1 |= 1 << (SPI_CR1_SPE_Pos);
+	SPI2->CR1 |= 1 << (SPI_CR1_CPOL_Pos);
+	SPI2->CR1 |= 1 << (SPI_CR1_CPHA_Pos);
 	
 }
 
 void init_gpio()
 {
-	//GPIO clock A, B, C, E
+	//GPIO clock A, B, C, H
 	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
-	//RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
+	RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
 	//RCC->IOPENR |= RCC_IOPENR_GPIOCEN;
 	//RCC->IOPENR |= RCC_IOPENR_GPIOHEN;
 	
-	//PA 0 for first ADC (add fnc)
-	GPIOA->MODER &= ~(GPIO_MODER_MODE0_Msk);
-	//GPIOA->MODER |= 1 << GPIO_MODER_MODE0_Pos;
+	// PB12, setting the select line as an output
+	GPIOB->MODER &= ~(GPIO_MODER_MODE12_Msk);
+	GPIOB->MODER |= 1 << (GPIO_MODER_MODE12_Pos);
 	
-	//PA 1 for second ADC (add fnc)
-	GPIOA->MODER &= ~(GPIO_MODER_MODE1_Msk);
-	//GPIOA->MODER |= 1 << GPIO_MODER_MODE1_Pos;
+	//PB13 set to alternate function (2), SPI clk
+	GPIOB->MODER &= ~(GPIO_MODER_MODE13_Msk);
+	GPIOB->MODER |= 2 << (GPIO_MODER_MODE13_Pos);
 	
-	//PA 2 for third ADC (add fnc)
-	GPIOA->MODER &= ~(GPIO_MODER_MODE2_Msk);
-	//GPIOA->MODER |= 1 << GPIO_MODER_MODE2_Pos;
+	//PB14 set to alt input (2), MISO
+	GPIOB->MODER &= ~(GPIO_MODER_MODE14_Msk);
+	GPIOB->MODER |= 2 << (GPIO_MODER_MODE14_Pos);
+	
+	//PB15 set to alt output (2), MOSI
+	GPIOB->MODER &= ~(GPIO_MODER_MODE15_Msk);
+	GPIOB->MODER |= 2u << (GPIO_MODER_MODE15_Pos);
 	
 	//PA 4 for DAC (only DAC output)(add fnc)
 	GPIOA->MODER &= ~(GPIO_MODER_MODE4_Msk);
