8b536547da2039f81d2cd6ebc023527f32f676e0
Dru walkowski <91984465+dawalkowski17@users.noreply.github.com>
2021-11-09 16:05:22 -0500

    spi read and write fnc
    I didn't want to rename the file
    the read function needs tweaked and read after the data is in from the spi
    note for later: need to write address before I can read data

---
+++ ADC_read.c
@@ -0,0 +1,12 @@
+// Reads the sensor because I can't read
+// puts into queue for other tasks
+// returns because, "I gotta go fast" 
+// -Ricky Bobby
+
+
+
+#include <stm32l053xx.h>
+#include <stdint.h>
+extern void init_adc(void);
+
+
