146832c127b0bcfe8ce743883982438877ad3186
Dru walkowski <91984465+dawalkowski17@users.noreply.github.com>
2021-11-11 18:41:42 -0500

    trigger and naming for accel
    like it says in the title

--- Main.c
+++ Main.c
@@ -5,11 +5,13 @@
 #include <stm32l053xx.h>
 #include <stdint.h>
 #include "init_everything.c"
+#include "ADC_read.c"
 
 //function calls (update as needed)
 
-//TODO: ADC_read fnc and its trigger (DRU)
-extern void ACC_read(void);
+//TODO: ADC_read fnc and its trigger (DRU)ADC
+extern void tim2(void);
+extern void accel_read(void);
 //TODO: threshold_compare fnc and trigger
 extern trigger_2();
 extern void threshold_compare(void);
@@ -24,17 +26,19 @@ extern trigger_5(); //if queue is full interupt (THIS MIGHT BE HARD)
 extern void sleepytime(void);
 //TODO: init fnc's
 extern void init_all(void);
+extern void x_y_z_r(void);
 
 
 int main(void)
 {
 	//TODO: intitionalization functions (DRU)
+	init_all();
 	//TODO: queue init
 	
 	while(1)
 	{
 		
-		if(trigger_1()){ACC_read();}
+		if(tim2()){accel_read();}
 		if(trigger_2()){threshold_compare();}   // semaphore for compare value
 		if(trigger_3()){play_sound();}          // also determines how loud
 		if(trigger_4()){change_sound();}
