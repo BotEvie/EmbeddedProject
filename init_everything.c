1a10fb7b3156f970fd323685a518edf0c3021f1e
Dru walkowski <91984465+dawalkowski17@users.noreply.github.com>
2021-11-11 22:29:00 -0500

    splicing Nate

--- init_everything.c
+++ init_everything.c
@@ -3,10 +3,11 @@
 #include <stm32l053xx.h>
 #include <stdint.h>
 
+
 void init_spi(void);
 void init_all(void);
 void init_gpio(void);
-void init_DAC(void);
+extern void init_DAC(void);
 void init_timer(void);
 void init_accel(void);
 extern void spiing_w(uint16_t data);
@@ -77,11 +78,35 @@ void init_gpio()
 	GPIOA->MODER &= ~(GPIO_MODER_MODE4_Msk);
 	//GPIOA->MODER |= 1 << GPIO_MODER_MODE4_Pos;
 	
+	GPIOA->MODER &= ~GPIO_MODER_MODE4_Msk;
+ 	GPIOA->MODER |= 1 << GPIO_MODER_MODE4_Pos;
+	
+	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
+  	GPIOA->MODER |= 1 << GPIO_MODER_MODE5_Pos;
+	
 }
 
 void init_DAC()
 {// DAC'ing to the speaker
+	RCC->APB1ENR |= RCC_APB1ENR_DACEN;   	    //Supply clock to DAC
 	
+	DAC->CR &= ~DAC_CR_WAVE1_Msk;
+	DAC->CR |= 2 << DAC_CR_WAVE1_Pos;           //Generate trinagle sound wave
+	
+	DAC->CR &= ~DAC_CR_MAMP1_Msk;
+	DAC->CR |= 9 << DAC_CR_MAMP1_Pos;           //Triangle has amplitude of 1023
+	
+	DAC->CR &= ~DAC_CR_BOFF1_Msk;
+	DAC->CR |= 1 << DAC_CR_BOFF1_Pos;           //Output buffer enable to avoid external op amp
+	
+	DAC->CR &= ~DAC_CR_TEN1_Msk;
+	DAC->CR |= 1 << DAC_CR_TEN1_Pos;            //Trigger enable to allow a triangle wave 
+	
+	DAC->CR &= ~DAC_CR_TSEL1_Msk;
+	DAC->CR |= 6 << DAC_CR_TSEL1_Pos;	    //Trigger on an timer 6
+	
+	DAC->CR &= ~DAC_CR_EN1_Msk;										
+	DAC->CR |= 1 << DAC_CR_EN1_Pos;	            //Enable DAC on channel 1
 }
 
 void init_timer()
@@ -93,9 +118,15 @@ void init_timer()
 	// overflow trigger for interupt only
 	TIM2->CR1 |= 1 << TIM_CR1_URS_Pos;
 	
+	//timer 6 for DAC
+	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+	
+	TIM6->CR1 &= ~TIM_CR1_CEN_Msk;
+	
 	
 	// start timer(s)
 	TIM2->CR1 |= 1 << TIM_CR1_CEN_Pos;
+	TIM6->CR1 |= 1 << TIM_CR1_CEN_Pos;
 }
 
 
