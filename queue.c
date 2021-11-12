e6238d48a8ba7cedc5b61ba871e9991d15d67628
BotEvie <91641915+BotEvie@users.noreply.github.com>
2021-11-11 20:41:50 -0500

    Update queue.c

--- queue.c
+++ queue.c
@@ -15,30 +15,32 @@ void init_queue(queue_t * ptr_to_queue, int max_items)
   ptr_to_queue ->max_index = max_items;
 }
 
-bool pop(queue_t *q0 int16_t *data)
+bool pop(queue_t *q0, int16_t *data)
 {
-  if(q0->head == q0->tail)  // if empty
-  { 
-      *data = 0;
-      success = false; 
-  }
-  else{
-         *data = (q0->buffer)[q0->head];
-         q0->head++;                      // move head to next location
-         if ((q0->head) > q0->max_index) q0->head=0;  // loop back around if exceeding max index
-         success = true;
+    bool success;
+    if(q0->head == q0->tail)  // if empty
+    { 
+        *data = 0;
+        success = false; 
     }
+    else{
+        *data = (q0->buffer)[q0->head];
+        q0->head++;                      // move head to next location
+        if ((q0->head) > q0->max_index) {q0->head=0;}  // loop back around if exceeding max index
+        success = true;
+    }
+    return success;
 }
 
-bool push(queue_t *q0, int16_t *data)
+bool push(queue_t *q0, int16_t data)
 {
-  uint16_t new_tail = q0->tail + 1;
-  if (new_tail > q0->max_index) {new_tail = 0}; // loop back around if going past max index
-  if (new_tail == q0->head) {return false}; //queue full = error
-  else{
-      (q0->buffer)[q0->tail] = data;  
-      q0->tail = new_tail;
-  }
+    uint16_t new_tail = q0->tail + 1;
+    if (new_tail > q0->max_index) {new_tail = 0; } // loop back around if going past max index
+    if (new_tail == q0->head) {return false; } //queue full = error
+    else{
+        (q0->buffer)[q0->tail] = data;  
+        q0->tail = new_tail;
+    }
     return true;
   
 }
