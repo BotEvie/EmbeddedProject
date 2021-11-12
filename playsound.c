f29f704f374a55fbfe176988d86d6e78aba1a01a
nsmatthews19 <91988490+nsmatthews19@users.noreply.github.com>
2021-11-12 00:10:15 -0500

    No More DAC. Using PWM. Change to TIM 22. Create functions to accept queue

--- playsound.c
+++ playsound.c
@@ -9,57 +9,39 @@
 
 
 
-void init_DAC(void);
-void init_timer(void);
-void init_pa4(void);
+#include "stm32l053xx.h"
 
-void init_DAC(){
-	
-	RCC->APB1ENR |= RCC_APB1ENR_DACEN;   	    //Supply clock to DAC
-	
-	DAC->CR &= ~DAC_CR_WAVE1_Msk;
-	DAC->CR |= 2 << DAC_CR_WAVE1_Pos;           //Generate trinagle sound wave
-	
-	DAC->CR &= ~DAC_CR_MAMP1_Msk;
-	DAC->CR |= 9 << DAC_CR_MAMP1_Pos;           //Triangle has amplitude of 1023
-	
-	DAC->CR &= ~DAC_CR_BOFF1_Msk;
-	DAC->CR |= 1 << DAC_CR_BOFF1_Pos;           //Output buffer enable to avoid external op amp
-	
-	DAC->CR &= ~DAC_CR_TEN1_Msk;
-	DAC->CR |= 1 << DAC_CR_TEN1_Pos;            //Trigger enable to allow a triangle wave 
-	
-	DAC->CR &= ~DAC_CR_TSEL1_Msk;
-	DAC->CR |= 6 << DAC_CR_TSEL1_Pos;	    //Trigger on an timer 6
-	
-	DAC->CR &= ~DAC_CR_EN1_Msk;										
-	DAC->CR |= 1 << DAC_CR_EN1_Pos;	            //Enable DAC on channel 1
-	
-}
 
-void init_timer(){
-	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
-	
-	TIM6->CR1 &= ~TIM_CR1_CEN_Msk;
-	TIM6->CR1 |= 1 << TIM_CR1_CEN_Pos;
-}
+//Using PWM instead of DAC now
 
-void init_pa4(){
-	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+int main(void)
+{
+	RCC-> IOPENR |= RCC_IOPENR_GPIOAEN;
+	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOA->MODER |= 2 << GPIO_MODER_MODE0_Pos;
+	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL0_Msk);
+	GPIOA->AFR[0] |= 2 << GPIO_AFRL_AFSEL0_Pos;
 	
-	GPIOA->MODER &= ~GPIO_MODER_MODE4_Msk;
-  GPIOA->MODER |= 1 << GPIO_MODER_MODE4_Pos;
-	
-	GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
-  GPIOA->MODER |= 1 << GPIO_MODER_MODE5_Pos;
-}
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
-int main(void){
-	init_pa4();
-	init_DAC();
-	init_timer();
+	TIM2->PSC = 100; /* (1) */ // Prescale divider
+	TIM2->ARR = 8; /* (2) */  // TOP
+	TIM2->CCR1 = 7; /* (3) */   // MATCH
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1
+	| TIM_CCMR1_OC1PE; /* (4) */
+	TIM2->CCER |= TIM_CCER_CC1E; /* (5) */
+	TIM2->CR1 |= TIM_CR1_CMS_0 | TIM_CR1_CEN; /* (6) */
+	TIM2->EGR |= TIM_EGR_UG; /* (7) */
 	
-	while(1){
-		
+	while (1){
+		for (volatile uint32_t n = 0; n < 12345; n++){}
 	}
 }
+
+
+//Plays sound at 1325 Hz
+
+//Need to configure the PWM on TIM22
+//Need to trigger PWM on input from queue
+//Implement these into functions without main. Accepts value from queue.
+
