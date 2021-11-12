2d54dd5fa91ce112c4ff84068599d94d83a5cf98
BotEvie <91641915+BotEvie@users.noreply.github.com>
2021-11-11 21:57:26 -0500

    Update Threshold.c

--- Threshold.c
+++ Threshold.c
@@ -11,9 +11,10 @@ int update_threshold_value(uint8_t toast);
 void threshold_fsm(queue_t *accel_queue, queue_t *value_queue);
 
 // Tested output accelerometer system total = T.O.A.S.T.
-// 
+
 int update_threshold_value(uint8_t toast, uint8_t *value)
 {
+	// we recieve the value from the accelerometer in two's complement
 	
 	// Higher numbers indicate higher acceleration, these are just random values with no real rhyme or reason
   	*value = (*value > 0)? *value: -(*value);
@@ -27,19 +28,19 @@ int update_threshold_value(uint8_t toast, uint8_t *value)
 // full state machine
 void threshold_fsm(queue_t *accel_queue, queue_t *value_queue)
 {
-	volatile int threshold_state;	// current state
-	volatile int threshold_max;	// highest recorded value this spike
+	static int threshold_state;	// current state
+	static int threshold_max;	// highest recorded value this spike
 	uint8_t value = 0;
-	uint8_t data=0;
+	uint8_t data = 0;
 	
 	pop(accel_queue, &data);
-	value = update_threshold_value(data);
+	update_threshold_value(data, &value);
 		
 	/* state: Motion the box is undergoing. Has 4 seperate values.
 	 	0: box is not accelerating.
 		1: box may be accelerating, wait for another reading.
 	 	2: box is accelerating. Start recording values to find max.
-		3: box may have decelarated below threshold. Await further input to confirm.
+		3: box may have decelarated below threshold. Await further input to confirm. Push max to queue if falling.
 	*/	
 	switch(threshold_state)
 	{
@@ -49,10 +50,10 @@ void threshold_fsm(queue_t *accel_queue, queue_t *value_queue)
 			else{ threshold_state = 0; }			
 			break;
 		case (2) : if(value < threshold_max){ threshold_state = 3; }
-			 if(threshold_max < value) {threshold_max = value;}		
+			 if(threshold_max < value) {threshold_max = value;}		// max less than value 
 			break;
-		case (3) : if(value < threshold_max){ threshold_state = 0; push(value_queue, threshold_max); }
-			if(value > threshold_max){ threshold_state = 2; threshold_max = value; }
+		case (3) : if(value < threshold_max){ threshold_state = 0; push(value_queue, threshold_max); threshold_max=0; }		// if value below max again spike is probably falling so push then clear max.
+			if(value > threshold_max){ threshold_state = 2; threshold_max = value; } 		// if value higher than max, spike still rising, record new value as max and retern to state 2
 			break;
 		default : state = 0;
 	}
