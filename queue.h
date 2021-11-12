b57c58425f9a1ec2b8d6636a35e88c93725557e5
BotEvie <91641915+BotEvie@users.noreply.github.com>
2021-11-11 21:26:42 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -5,15 +5,15 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef struct {int16_t * buffer;
-                uint16_t head;
-                uint16_t tail;
-                uint16_t max_index;
+typedef struct {uint8_t * buffer;
+                uint8_t head;
+                uint8_t tail;
+                uint8_t max_index;
                } queue_t;
 
 void init_queue(queue_t * ptr_to_queue, int max_items);
-bool pop(queue_t *q0, int16_t *data);
-bool push(queue_t *q0, int16_t data);
+bool pop(queue_t *q0, uint8_t *data);
+bool push(queue_t *q0, uint8_t data);
 
    
 #endif // QUEUE_H_
