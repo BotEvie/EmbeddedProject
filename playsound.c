02f3fd32f7d00b8dc96557ae29e1bb605ff4b686
nsmatthews19 <91988490+nsmatthews19@users.noreply.github.com>
2021-11-14 21:14:02 -0500

    Functions are able to be called to start and stop sound

--- playsound.c
+++ playsound.c
@@ -1,47 +1,32 @@
 //Nathan Matthews
 //
-//Play sound to speaker using DAC.
+//Play sound to speaker using PWM.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "stm32l0xx.h"                  // Device header
-
-
-
 #include "stm32l053xx.h"
 
 
-//Using PWM instead of DAC now
+void sound_on(void);
+void sound_off (void);
 
-int main(void)
-{
-	RCC-> IOPENR |= RCC_IOPENR_GPIOAEN;
-	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
-	GPIOA->MODER |= 2 << GPIO_MODER_MODE0_Pos;
-	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL0_Msk);
-	GPIOA->AFR[0] |= 2 << GPIO_AFRL_AFSEL0_Pos;
-	
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-
-	TIM2->PSC = 100; /* (1) */ // Prescale divider
-	TIM2->ARR = 8; /* (2) */  // TOP
-	TIM2->CCR1 = 7; /* (3) */   // MATCH
-	TIM2->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1
-	| TIM_CCMR1_OC1PE; /* (4) */
-	TIM2->CCER |= TIM_CCER_CC1E; /* (5) */
-	TIM2->CR1 |= TIM_CR1_CMS_0 | TIM_CR1_CEN; /* (6) */
-	TIM2->EGR |= TIM_EGR_UG; /* (7) */
-	
-	while (1){
-		for (volatile uint32_t n = 0; n < 12345; n++){}
-	}
+void sound_on(){
+	RCC->APB2ENR |= RCC_APB2ENR_TIM22EN;
+	TIM22->PSC = 100;                                              // Prescale to make 1325 Hz
+	TIM22->ARR = 8;                                                
+	TIM22->CCR1 = 7;                                             
+	TIM22->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1
+	| TIM_CCMR1_OC1PE; 
+	TIM22->CCER |= TIM_CCER_CC1E; 
+	TIM22->CR1 |= TIM_CR1_CMS_0 | TIM_CR1_CEN;                    //CEN bit turns on the counter to start the sound
+	TIM22->EGR |= TIM_EGR_UG; 
 }
 
+void sound_off(){
+	TIM22->CR1 &= ~TIM_CR1_CEN;                                   //CEN bit turns off the counter to stop the sound
+}
 
-//Plays sound at 1325 Hz
-
-//Need to configure the PWM on TIM22
-//Need to trigger PWM on input from queue
-//Implement these into functions without main. Accepts value from queue.
-
+//PA6 is configured to TIM22 in init_everything task
+//These 2 functions should be called with different triggers.
+//One function enables the PWM, one disables.
