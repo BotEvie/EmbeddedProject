306162ccb56e4ed38fe9ced7dd96523d5764dad3
Dru walkowski <91984465+dawalkowski17@users.noreply.github.com>
2021-11-16 09:12:03 -0500

    fake reading
    due to board malfunctions I am currently just trying to integrate so that we can still run a demo
    the data into the read fnc is a mask so that it will spit out numbers for us that resemble practical values

--- accel_read.c
+++ accel_read.c
@@ -18,19 +18,22 @@ void x_y_z_r(void);
 
 void spiing_r(uint8_t data_out)
 {//reading the accelerometer
+	//currently fake reading
+	
+	uint8_t data_in = rand & data_out;
 	
 	//PB12 goes low in ODR
-	GPIOB->ODR &= ~(1u << 12);
+	//GPIOB->ODR &= ~(1u << 12);
 	//sending register location
-	SPI2->DR |= data_out;
+	//SPI2->DR |= data_out;
 	//MISO puts data in SPI DR then to queue
-	uint8_t data_in = ((SPI2->DR) & 0x000F); 
+	//uint8_t data_in = ((SPI2->DR) & 0x000F); 
 	push(&q_t,data_in);
 	//wait for SPI to finish getting data
 	//for(volatile int32_t n = 0; n < 3; n++){}
-	while(SPI2->SR & SPI_SR_BSY){}
+	//while(SPI2->SR & SPI_SR_BSY){}
 	//PB12 goes high in ODR
-	GPIOB->ODR |= 1 << 12;
+	//GPIOB->ODR |= 1 << 12;
 }
 
 
@@ -41,7 +44,17 @@ void spiing_w(uint16_t data_out)
 	//PB12 goes low in ODR
 	GPIOB->ODR &= ~(1u <<12);
 	//MOSI pulls data from SPI DR
-	SPI2->DR |= data_out;
+	SPI2->DR |= 8 >> (data_out & 0xFF00);
+	//wait for SPI to finish sending data
+	//for(volatile int32_t n = 0; n < 3; n++){}
+	while(SPI1->SR & SPI_SR_BSY){}
+	//PB12 goes high in ODR
+	GPIOB->ODR |= 1 << 12;
+	
+	//PB12 goes low in ODR
+	GPIOB->ODR &= ~(1u <<12);
+	//second half of data
+	SPI2->DR |= data_out & 0x00FF;
 	//wait for SPI to finish sending data
 	//for(volatile int32_t n = 0; n < 3; n++){}
 	while(SPI1->SR & SPI_SR_BSY){}
@@ -54,11 +67,12 @@ void x_y_z_r()
 	//should take < 8 us to make a read for 8bit read from accel
 	
 	//x axis read
-	spiing_r(0x09);
+	//spiing_r(0x09);
+	spiing_r(0x00)
 	//y axis read
-	spiing_r(0x0B);
+	spiing_r(0x00);
 	//z axis read
-	spiing_r(0x0D);
+	spiing_r(0x6A);
 	// reseting the timer flag cause we done
 	TIM2->SR |= TIM_SR_UIF_Msk << TIM_SR_UIF_Pos;
 	
