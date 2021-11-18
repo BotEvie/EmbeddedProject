b0e41dd8c039df60ed96201eef7e8e6da68a06b3
nsmatthews19 <91988490+nsmatthews19@users.noreply.github.com>
2021-11-17 22:50:26 -0500

    Ready for demo

--- playsoundd.c
+++ playsoundd.c
@@ -3,6 +3,7 @@
 //Play sound to speaker using PWM.
 //
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "stm32l053xx.h"
@@ -11,23 +12,14 @@
 void sound_on(void);
 void sound_off (void);
 
+//Functions alternate whether channel 1 for TIM22 is active or inactive
+
 void sound_on(){
-	RCC->APB2ENR |= RCC_APB2ENR_TIM22EN;
-	TIM22->PSC = 100;                                              // Prescale to make 1325 Hz
-	TIM22->ARR = 8;                                                
-	TIM22->CCR1 = 7;                                             
-	TIM22->CCMR1 |= 6 << TIM_CCMR1_OC1M_Pos;
-	TIM22->CCMR1 |= 1 << TIM_CCMR1_OC1PE; 
-	TIM22->CCER |= 1 << TIM_CCER_CC1E; 
-	TIM22->CR1 |= 1 << TIM_CR1_CMS_Pos; 
-	TIM22->CR1 |= 1 << TIM_CR1_CEN;                    //CEN bit turns on the counter to start the sound
-	TIM22->EGR |= 1 << TIM_EGR_UG; 
-	
-	for(volatile int32_t n = 0; n < 7000; n++){}
+	TIM22->CCER |= TIM_CCER_CC1E; 
 }
 
 void sound_off(){
-	TIM22->CCER &=  ~(TIM_CCER_CC1E_Msk);                                   //CEN bit turns off the counter to stop the sound
+	TIM22->CCER &=  ~(TIM_CCER_CC1E_Msk);                                 
 }
 
 //PA6 is configured to TIM22 in init_everything task
