fcfc1784b33111fe78e34bec843bf1719c2801db
nsmatthews19 <91988490+nsmatthews19@users.noreply.github.com>
2021-11-17 22:46:21 -0500

    PWM initialized in init_everything task

--- playsound.c
+++ playsound.c
@@ -27,15 +27,7 @@ void sound_on(void);
 void sound_off (void);
 
 void sound_on(){
-	RCC->APB2ENR |= RCC_APB2ENR_TIM22EN;
-	TIM22->PSC = 100;                                              // Prescale to make 1325 Hz
-	TIM22->ARR = 8;                                                
-	TIM22->CCR1 = 7;                                             
-	TIM22->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1
-	| TIM_CCMR1_OC1PE; 
 	TIM22->CCER |= TIM_CCER_CC1E; 
-	TIM22->CR1 |= TIM_CR1_CMS_0 | TIM_CR1_CEN;                    //CEN bit turns on the counter to start the sound
-	TIM22->EGR |= TIM_EGR_UG; 
 }
 
 void sound_off(){
