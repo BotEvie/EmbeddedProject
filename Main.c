03b7c2f65d50ed5612e1ba944751921c7b91dc3b
Dru walkowski <91984465+dawalkowski17@users.noreply.github.com>
2021-11-22 15:42:52 -0500

    matching working demo code
    copy and paste baby

--- Main.c
+++ Main.c
@@ -19,52 +19,106 @@ IN THE SOFTWARE.
 
 #include <stm32l053xx.h>
 #include <stdint.h>
-#include "init_everything.c"
-#include "accel_read.c"
-#include "playsound.c"
-#include "queue.c"
+#include <stdbool.h>
+/*
+#include "init_everythingd.c"
+#include "accel_readd.c"
+#include "playsoundd.c"
+#include "queued.c"
+#include "thresholdd.c"
+*/
+#include "queued.h"
 
-//function calls (update as needed)
 
-//accelerometer read function and the trigger is a timer interupt from TIM2
-extern void x_y_z_r(void);
-//threshold_compare fnc and trigger
-//trigger is from yeet_queue
-extern bool cq_stuff(queue_t *q0);
-extern void threshold_compare(void);
-//sound_on fnc and trigger (NATE)
-//trigger is cq_stuff, but with highest queue
-extern void sound_on(void);
-//TODO: sleepytime fnc
-extern void sleepytime(void);
-//TOD0: init fnc's
+extern void update_threshold_value(uint16_t toast, uint16_t *value);
+extern void threshold_compare(queue_t *accel_queue, queue_t *value_queue);
+extern void spiing_r(int data, queue_t *q0);
+extern void spiing_w(uint16_t data);
+extern void x_y_z_r(queue_t *q0);
 extern void init_all(void);
+extern void init_gpio(void);
+extern void init_timer(void);
+extern void init_accel(void);
+extern void sound_on(void);
+extern void sound_off (void);
+extern bool cq_full(queue_t *q0);
+extern bool cq_stuff(queue_t *q0);
+
+
+
+void sleepytime(queue_t *q0, queue_t *q1);
+
+
+
+// queues
+queue_t yeet_queue;
+queue_t highest;
+queue_t killer;
+queue_t n;
+
+
+void sleepytime(queue_t *q0, queue_t *q1)
+{	push(&killer, 0x01);
+	uint16_t num;
+	pop(&n,&num);
+	if(num >= 12345u)
+	{
+		sound_off();
+		while(true == cq_stuff(q0))
+		{// empty the queue
+			
+			if(num == 1)
+			{	uint16_t garbage;
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+				pop(q0, &garbage);
+		
+				pop(&killer, 0x00);
+			}
+			num--;
+			//push(&n,num);
+		}
+		pop(&n,&num);
+	}
+	num+=3;
+	push(&n,num);
+}
 
 
 
 int main(void)
 {
-	//TODO: intitionalization functions (DRU)
+	//all initialization fncs
 	init_all();
-	//TODO: queue init
 	
+	// queue init
+	init_queue(&yeet_queue, 6);
+	init_queue(&highest, 2);
+	init_queue(&killer, 1);
+	init_queue(&n, 1);
 	
 	while(1)
 	{
 		
-		if(TIM_SR_UIF == 1){x_y_z_r();}
-		if(cq_stuff(&yeet_queue) == true){threshold_compare();}   // semaphore for compare value
+		if((TIM_SR_UIF == 1) && !(cq_stuff(&killer))){x_y_z_r(&yeet_queue);}
+		if((cq_stuff(&yeet_queue)) && !(cq_stuff(&killer))){threshold_compare(&yeet_queue, &highest);}   // semaphore for compare value
 		// bleow tasks are sequential, but different authors
 		// after playing sound we are using sleepytime as alt. schedule
 		// sleepytime only runs above 2 tasks until yeet_queue is empty
-		if(cq_stuff(&highest) == true)
-		{	uint32_t interruptible = __get_primask();
+		if(cq_stuff(&highest) | cq_stuff(&killer))
+		{	uint32_t interruptible = __get_PRIMASK();
 		 	__disable_irq();
-			sound_on();		 // also determines how loud
-			sleepytime();		 // puts to sleep until we slowly empty the queue
-		 	__set_primask(interuptible);
+			if(cq_stuff(&killer) == false){sound_on();}		 // also determines how loud
+			sleepytime(&yeet_queue, &killer);		 // puts to sleep until we slowly empty the queue
+		 	__set_PRIMASK(interruptible);
 		}           
-		        
+		  
 		
 	}
 
